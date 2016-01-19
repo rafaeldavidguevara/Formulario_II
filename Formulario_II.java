8b21c17c02f86f8dc83a21f955cc54808ad1e42a
RD <rafaeldavidguevara@gmail.com>
2016-01-19 19:13:01 -0430

    update

--- Formulario_II.java
+++ Formulario_II.java
@@ -9,6 +9,8 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Dimension;
 import java.awt.ComponentOrientation;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -16,33 +18,61 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JFrame;
 
-public class Formulario_II extends JPanel {
+public class Formulario_II extends JPanel implements ActionListener{
   private static String cadenas[] = {"First Name", "Last Name", "Address","Salary"};
   private static String cadenas_b[] = {"Insert", "Update", "Delete","Clear"};
+  private JTextField campos_t[] = new JTextField[cadenas.length];
+  private JLabel etiquetas[] = new JLabel[cadenas.length];
+  private JButton botones[] = new JButton[4];
   private GridBagConstraints constraints = new GridBagConstraints();	
     
-  private JPanel crearPanel(int campos){
+  private JPanel crearPanel(int campos, boolean label){
     JPanel panel = new JPanel();
     panel.setLayout(new GridBagLayout());
-	int k = 0,l = 0;
     for ( int i = 0; i< campos; i++ ){
       JLabel etiqueta = new JLabel(cadenas[i]+":");  
-      setNewConstraints(0, l, 1, 1, 1, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL);
+      setNewConstraints(0, i, 1, 1, 1, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL);
       panel.add(etiqueta, constraints);
-      setNewConstraints(2, l, 1, 1, 1, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL);
-      panel.add(new JTextField(), constraints);
-      l++;  		  
+      setNewConstraints(2, i, 1, 1, 1, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL);
+      if (label){
+		etiquetas[i] = new JLabel();     
+        panel.add(etiquetas[i], constraints);
+      }
+	  else{
+        campos_t[i] = new JTextField();		  
+	    panel.add(campos_t[i], constraints);
+      }		 		  
     }
 	return panel;	
+  }
+  
+  public void actionPerformed( ActionEvent evt ){
+    JButton boton = (JButton) evt.getSource();
+	String aux = boton.getText();
+	if (aux.equals("Insert")){
+      for (int i=0; i<4; i++)
+        etiquetas[i].setText(campos_t[i].getText()); 	  
+	}else if(aux.equals("Delete")){
+      for (int i=0; i<4; i++)
+        etiquetas[i].setText(""); 	
+    }else if(aux.equals("Clear")){
+      for (int i=0; i<4; i++)
+        campos_t[i].setText(""); 	
+    }    
   }
   
   private JPanel crearPanel(){
     JPanel panel = new JPanel();
     panel.setLayout(new FlowLayout(FlowLayout.LEFT));
 	for (int i = 0; i<cadenas_b.length; i++){
-      JButton button = new JButton(cadenas_b[i]);
-      button.setPreferredSize(new Dimension(80, 25));
-      panel.add(button);   
+	  botones[i] = new JButton(cadenas_b[i]);
+      botones[i].setPreferredSize(new Dimension(80, 25));
+	  switch (i){
+	    case 0: botones[i].addActionListener(this);break;	  
+		case 2: botones[i].addActionListener(this);break;	    
+		case 3: botones[i].addActionListener(this);	  
+	  }
+      panel.add(botones[i]);   
     }	
 	return panel;	
   }
@@ -73,9 +103,9 @@ public class Formulario_II extends JPanel {
   
   public Formulario_II() {
     setLayout(new BoxLayout(this ,BoxLayout.Y_AXIS));
-    add(crearPanel(cadenas.length));
+    add(crearPanel(cadenas.length, false));
 	add(crearPanel());
-	add(crearPanel(cadenas.length));	
+	add(crearPanel(cadenas.length, true));	
 	add(crearPanelInf());    
   }
 
